d8c9d2df0c6d3942a010bb4538c712651c628662
Kishore kumar <kk78744@gmail.com>
2019-02-08 10:18:52 +0530

    created Login Component

--- learning/src/app/app.module.ts
+++ learning/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Route} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
-
+const myRoutes: Route[] = [
+  {
+    path:'',
+    redirectTo: '/login',
+    pathMatch:'full'
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(myRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
