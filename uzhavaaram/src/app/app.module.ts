56c0513bdc18812d51041019b64a767b6ea5c534
KISHORE KUMAR <kk78744@gmail.com>
2019-12-09 01:03:04 +0530

    # Main view component - Included angular material and fonts for application (#9)
    * #main-view-component - added the component for landing page and include the fonts, angular material and css required for building the application
    
    * #main-view-component - included angular material

--- uzhavaaram/src/app/app.module.ts
+++ uzhavaaram/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule, PreloadAllModules } from '@angular/router';
 import { AppRoutes } from './app.constants';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
@@ -13,6 +14,7 @@ import { AppRoutes } from './app.constants';
     imports: [
         BrowserModule,
         RouterModule.forRoot(AppRoutes, { useHash: true, preloadingStrategy: PreloadAllModules }),
+        BrowserAnimationsModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
